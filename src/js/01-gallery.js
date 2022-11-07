8dee295a9318fd82a3c81ef64f7b6836517183b0
Svitlana Synytsia <svetlankogr@gmail.com>
2022-11-07 16:12:26 +0100

    settings json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup();
+
+gallery.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+function createGalleryItemsMarkup() {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+    `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});

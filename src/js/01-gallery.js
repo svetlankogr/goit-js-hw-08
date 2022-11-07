9c6bee6b8bd6804fec82fbdb578bf9e5dd1d5680
Svitlana Synytsia <svetlankogr@gmail.com>
2022-11-07 16:21:51 +0100

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,5 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');

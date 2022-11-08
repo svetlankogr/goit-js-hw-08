85043a31f25d0ce4f053dd1cc61b159a7435776f
Svitlana Synytsia <svetlankogr@gmail.com>
2022-11-08 17:33:43 +0100

    02task video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+playbackToStart();
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(currentTime) {
+  localStorage.setItem(STORAGE_KEY, currentTime.seconds);
+}
+
+function playbackToStart() {
+  const currentTime = localStorage.getItem(STORAGE_KEY);
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+}
